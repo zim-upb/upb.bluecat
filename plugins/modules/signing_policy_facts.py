aaea408a3c7476503161a59477ef046bef457ab1
Philipp Fromme <philipp.fromme@uni-paderborn.de>
2026-04-22 16:43:22 +0200

    Add signing_policy_facts module

---
+++ plugins/modules/signing_policy_facts.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2026, Philipp Fromme <philipp.fromme@uni-paderborn.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible_collections.local.bluecat.plugins.module_utils.bc_util import BluecatModule
+
+class SigningPolicyFacts(BluecatModule):
+    def __init__(self):
+        self.module_args = dict()
+
+        super(SigningPolicyFacts, self).__init__(self.module_args,
+                                                 supports_check_mode=True,
+                                                 is_fact=True)
+
+    def exec_module(self, **kwargs):
+        results = dict(ansible_facts=dict(signing_policies=[]))
+        response = self.client.http_get('/signingPolicies',
+                                        params={'limit': self.module.params.get('limit'),
+                                                'filter': self.module.params.get('filter'),
+                                                'fields': self.module.params.get('fields')
+                                                }
+                                        )
+        if response['count'] > 0:
+            signing_policies= response['data']
+            results['ansible_facts']['signing_policies'] = signing_policies
+        return results
+
+def main():
+    SigningPolicyFacts()
+
+if __name__ == '__main__':
+    main()
